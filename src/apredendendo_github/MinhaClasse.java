fb44642e959494bea9bb1b242c7618645efb0f85
Eduardo DAddario <edudaddario@gmail.com>
2021-08-10 15:17:12 -0300

    versão 1.00

---
+++ src/apredendendo_github/MinhaClasse.java
@@ -0,0 +1,12 @@
+package apredendendo_github;
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Minha primeira classe Java versão 1.00");
+
+	}
+
+}

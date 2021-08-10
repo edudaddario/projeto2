3cdc9f08c735e9bd9fb91ea44b883bead28cfd90
edudaddario <edudaddario@gmail.com>
2021-08-10 15:49:07 -0300

    Update MinhaClasse.java

--- src/apredendendo_github/MinhaClasse.java
+++ src/apredendendo_github/MinhaClasse.java
@@ -1,11 +1,14 @@
 package apredendendo_github;
 
+import java.awt.datatransfer.SystemFlavorMap;
+
 public class MinhaClasse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Minha primeira classe Java versão 1.00");
+		System.out.println("Novo Commit");
 		// TESTE TESTE
 
 	}
